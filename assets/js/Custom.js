31dd5612c39443a56397f0ac783570e62a07839b
Alan <70604390+Formal-117@users.noreply.github.com>
2020-12-12 21:33:44 +0300

    Update Custom.js

--- assets/js/Custom.js
+++ assets/js/Custom.js
@@ -24,6 +24,10 @@ var gold_title = document.getElementById("gold-title");
 var bronzeTable = document.getElementById("bronze-table");
 var goldTable = document.getElementById('gold-table');
 var silverTable = document.getElementById('silver-table');
+var pay = document.getElementById("pay");
+var gold_titleDuo = document.getElementById("gold-title-2")
+var silver_titleDuo = document.getElementById("silver-title-2");
+var bronze_titleDuo = document.getElementById("bronze-title-2");
 
 gold_title.onclick = function () {
 	console.log("gold");
@@ -31,6 +35,17 @@ gold_title.onclick = function () {
 	silverTable.style.display = "none";
 	goldTable.style.display = 'block';
 	goldTable.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+	pay.style.display = "block";
+
+}
+
+gold_titleDuo.onclick = function () {
+	console.log("gold");
+	bronzeTable.style.display = 'none';
+	silverTable.style.display = "none";
+	goldTable.style.display = 'block';
+	goldTable.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+	pay.style.display = "block";
 
 }
 
@@ -40,6 +55,16 @@ silver_title.onclick = function () {
 	bronzeTable.style.display = 'none';
 	silverTable.style.display = "block";
 	silverTable.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+	pay.style.display = "block";
+}
+
+silver_titleDuo.onclick = function () {
+	console.log("silver");
+	goldTable.style.display = 'none';
+	bronzeTable.style.display = 'none';
+	silverTable.style.display = "block";
+	silverTable.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+	pay.style.display = "block";	
 }
 
 bronze_title.onclick = function () {
@@ -48,10 +73,17 @@ bronze_title.onclick = function () {
 	silverTable.style.display = 'none';
 	bronzeTable.style.display = "block";
 	bronzeTable.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+	pay.style.display = "block";
 }
 
-
-
+bronze_titleDuo.onclick = function () {
+	console.log("bronze");
+	goldTable.style.display = 'none';
+	silverTable.style.display = 'none';
+	bronzeTable.style.display = "block";
+	bronzeTable.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+	pay.style.display = "block";
+}
 
 
 
